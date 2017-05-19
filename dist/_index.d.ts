08d897eceff051e6ae17d7f49795e7d268533147
Jason <JasonS@Novaleaf.com>
2017-05-19 13:47:08 -0700

    simplify dist folder to use source typescript files directly (no .d.ts generation needed)

--- dist/_index.d.ts
+++
@@ -1,2 +0,0 @@
-import proxy = require("./proxy");
-export = proxy;
--- dist/middleware/wildcard.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * group1: subdomain
- * group2: domain.ext
- * exclude short domains (length < 4) to avoid catching double extensions (ex: net.au, co.uk, ...)
- */
-declare const HOSTNAME_REGEX: RegExp;
